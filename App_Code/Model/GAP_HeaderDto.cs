582f8d31e1eb1f07c4ebd2526a8b1e9e18681cef
pdftag <112856702+pdftag@users.noreply.github.com>
2022-09-06 13:41:21 +0800

    Add files via upload

---
+++ App_Code/Model/GAP_HeaderDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// GAP_HeaderDto 的摘要描述
+/// </summary>
+public class GAP_HeaderDto
+{
+    public long luhid { get; set; }
+    public long pipid { get; set; }
+    public string season { get; set; }
+    public string style { get; set; }
+    public string stylestatus { get; set; }
+    public string ProductDescription { get; set; }
+    public string styledesc { get; set; }
+    public string productsize { get; set; }
+    public string brand { get; set; }
+    public string dividion { get; set; }
+    public string sclass { get; set; }
+    public string pod { get; set; }
+    public string generateddate { get; set; }
+    public string stylesketch { get; set; }
+    public string creator { get; set; }
+
+}
---
+++ App_Code/Model/GAP_LearnmgrItemDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Lu_LearnmgrItem 的摘要描述
+/// </summary>
+public class GAP_LearnmgrItemDto
+{
+    public long liid { get; set; }
+    public string ColSource { get; set; }
+    public string ColName { get; set; }
+    public string FirstCharTermname_org { get; set; }
+    public string Termname_org { get; set; }
+    public string Termname { get; set; }
+
+}
